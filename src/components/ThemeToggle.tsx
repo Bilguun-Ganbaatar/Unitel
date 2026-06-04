71e5f646c3addbcc43f02c3d50503d5f78e585cc
Bilguun <bilguunganbaatar29@gmail.com>
2026-06-04 15:49:36 +0800

    light and dark

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,23 +1,36 @@
 "use client";
 
-import { Switch, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { Switch, rem, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
-  const computedColorScheme = useComputedColorScheme("light", { getInitialValueInEffect: true });
+  const computedColorScheme = useComputedColorScheme("light", {
+    getInitialValueInEffect: true,
+  });
+
   const [mounted, setMounted] = useState(false);
-  const isDark = mounted && computedColorScheme === "dark";
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
+  const isDark = mounted && computedColorScheme === "dark";
+
   return (
     <Switch
-      defaultChecked
-      color="cyan"
+      checked={isDark}
       onChange={() => setColorScheme(isDark ? "light" : "dark")}
+      color="#0bb0c1"
+      size="md"
+      thumbIcon={
+        isDark ? (
+          <IconMoonStars style={{ color: "gray", width: rem(12), height: rem(12) }} stroke={2.5} />
+        ) : (
+          <IconSun style={{ width: rem(12), height: rem(12) }} stroke={2.5} />
+        )
+      }
     />
   );
 }
